65c0b6a870d71a116002099419d08baeeb38c00f
Ajaya1000 <ajaymati1000@gmail.com>
2021-03-17 13:44:57 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# styled-component-template
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/styled-component-template)
---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import styled, { css } from "styled-components";
+import "./style.css";
+const css1 = css`
+  width: 100px;
+  height: 100px;
+  background: #000;
+`;
+const css2 = css`
+  ${css1};
+  background: pink;
+`;
+const StyledDiv = styled.div`
+  ${css2}
+`;
+
+export default function App() {
+  return <StyledDiv> </StyledDiv>;
+}
